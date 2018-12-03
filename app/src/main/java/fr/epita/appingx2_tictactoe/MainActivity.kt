3be6a4bf6a983aa1f24f7764911a2a7437bbd007
AnonymousIsTheodore <theodore.guillot@gmail.com>
2018-12-03 15:58:33 +0100

    Home done and going to second act

--- app/src/main/java/fr/epita/appingx2_tictactoe/MainActivity.kt
+++ app/src/main/java/fr/epita/appingx2_tictactoe/MainActivity.kt
@@ -1,12 +1,39 @@
 package fr.epita.appingx2_tictactoe
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_start.setOnClickListener(this@MainActivity)
+    }
+
+    override fun onClick(clickedView: View?) {
+        if (clickedView != null) {
+            when (clickedView.id) {
+                R.id.button_start -> {
+                    goToSecondActivity()
+                    Log.d("MainActivity/TextCount", "text_count increased")
+                }
+                else -> {
+                    Log.d("MainActivity", "onClick -> clickedView -> else reached")
+                }
+            }
+        }
+    }
+
+    private fun goToSecondActivity() {
+        val explicitIntent = Intent(this, SecondActivity::class.java)
+        val message = "Hello from another world"
+        explicitIntent.putExtra("MESSAGE", message)
+        startActivity(explicitIntent)
     }
 }
